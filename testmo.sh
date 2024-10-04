1cdb29ab679b22cd5b5a73a625eb34634a870b1d
Roberto Araneda <robaraneda@gmail.com>
2024-10-04 12:04:30 -0300

    Update testmo.sh

--- testmo.sh
+++ testmo.sh
@@ -4,7 +4,7 @@
 #npx testmo automation:resources:add-link --name build--url $BUILD_URL --resources resources.json
 
 # Run automated tests and report results to Testmo
-export TESTMO_TOKEN="testmo_api_eyJpdiI6IndGK0N2N2ZFbEZ4ZkF2YlJDN2lSUmc9PSIsInZhbHVlIjoiNzZBRkVkNDlRNVR2Q0Y5STFHQThGQnZaQnJlTlMxa2VvWnJoY3ZNZUM4Yz0iLCJtYWMiOiI3YTBlYjIyMzRiNjNiYzM3NWZlMzU1NDI3NzczMjU1NzY0Zjc5MzNmNmRkMDM3MjQyZWIwMTg0ZGExNDY2MTY3IiwidGFnIjoiIn0="
+export TESTMO_TOKEN="no-token"
 TESTMO_URL="https://falp.testmo.net"
 npx testmo automation:run:submit \
     --instance "$TESTMO_URL" \
